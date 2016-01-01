e09e0e180afd9d5fce8190fbfabc2ed54402a635
Madan Kapoor <madankapoor10@gmail.com>
2016-01-01 17:02:13 +0530

    Update solution.c
    Added c code for even case

--- solution.c
+++ solution.c
@@ -3,6 +3,14 @@
 #define MAXSIZE 1000000
 char k[MAXSIZE+2];
 
+int twopalindromehandler(int i,int j) // returns 0 on no carry
+// returns 1 on carry
+{
+
+
+	
+}
+
 int main()
 {
 	int t=0;
@@ -14,15 +22,14 @@ int main()
 	}
 	while(t--)
 	{	
-		int isnineseries=1,ispalindrome,i=0,length;
+		int isnineseries=1,ispalindrome=1,i=0,length;
 		c=getchar();
 		while( c >= '0' && c <= '9')
 		{	
 			if( c =='9' ) 
 				isnineseries=1;
 			else 
-				isnineseries=0;
-			
+				isnineseries=0;		
 			k[i++]=c;
 			c=getchar();
 		}
@@ -39,13 +46,22 @@ int main()
 		}
 		else if(length%2==0)
 		{
+			int i=length/2,j=length/2+1,carryset=0;
+			while( i >= 0 && j < length )
+			{
+				if((k[i]-k[j])!=0)
+				{	ispalindrome=0;
+					twopalindromehandler(i,j);
+				}
+			}
 			
-			puts(k);
 		}
 		else
 		{
 			
 		}
+		puts(k);//solution
 	}
 	return 0;
+	//git@github.com:Madankapoor/NumberPalindrome.git
 }
